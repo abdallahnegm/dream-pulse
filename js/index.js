87240e5bff056aa20111a82d746d6ca5a5fea52e
abdallahnegm <73608714+abdallahnegm@users.noreply.github.com>
2022-12-01 04:41:31 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+function newfun() {
+    var btn=document.getElementById("mybtn")
+    var men=document.getElementById("left")
+    if(men.style.left="-200%"){
+        men.style.left="-100px";
+    }
+    else {
+        men.style.left="-200%" ;
+    }
+}
+function exite(){
+    ex=document.getElementById("left")
+    if(ex.style.left="-100px"){
+        ex.style.left="-200%";
+    }
+    else {
+        ex.style.left="block" ;
+    }
+}
+// window.onload
+setTimeout( function load(){
+    var ll=document.getElementById("load") 
+    if(ll.style.display="block"){
+        ll.style.display="none"
+    }
+    else{
+        ll.style.display="none"
+    }
+},) 
+// setTimeout(function(){
+//     $('body')
+//     .removeClass('loading')
+//     .addClass('loaded')
+// })
+// window.load=load()
+// setTimeout(function() {
+//     $('body')
+//         .removeClass('loading')
+//         .addClass('loaded');
+// },2000);
